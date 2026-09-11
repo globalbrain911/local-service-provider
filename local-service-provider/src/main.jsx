455a6160e7e3bce6aa1134b0361c7d3231fc9f8f
globalbrain911 <globalbrain911@gmail.com>
2026-09-11 10:59:16 +0530

    add routing

--- local-service-provider/src/main.jsx
+++ local-service-provider/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./About.jsx";
+import NotFoundPage from "./NotFound.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/About", element: <About /> },
+  { path: "*", element: <NotFoundPage /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
